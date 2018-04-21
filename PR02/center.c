01d46b0673a6047aab0e86be3dfaec0b5c6dd8ce
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-04-20 21:21:39 -0300

    Second Iteraction
    Adjustments to printf so that each line is properly centered.
    
    Add functionality to write to a output file and receive col_limit from
    the command line, along with <input_file> <output_file>.
    
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- PR02/center.c
+++ PR02/center.c
@@ -32,15 +32,21 @@ char *trimwhitespace(char *str) {
 int main (int argc, char** argv) {
   Buffer *b = buffer_create(sizeof(char));
   FILE* input_text = fopen(argv[1], "r");
+  FILE* output_file = fopen(argv[2], "w");
+  int col_limit = atoi(argv[3]);
 
   while(!is_over) {
     chars_in_line =  read_line(input_text, b);
     if (chars_in_line == 0) is_over = 1;
-    printf("%d\n", chars_in_line);
+    // printf("%d\n", chars_in_line);
 
     current_line = b->data;
     current_line = trimwhitespace(current_line);
-    printf("%s\n", current_line);
+
+    int line_length = strlen(current_line);
+
+    // printf("%d\n", line_length);
+    fprintf (output_file,"%*s\n", col_limit / 2 + line_length / 2, current_line);
   }
 
   return 0;
