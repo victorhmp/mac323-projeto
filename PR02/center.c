84c57535e922f0b8edba66d4340de2e6c852af6c
Civile <civile@Civile.local>
2018-04-24 15:57:07 -0300

    Double blank lines deletedDouble blank lines deleted..

--- PR02/center.c
+++ PR02/center.c
@@ -12,12 +12,25 @@ int chars_in_line = 0;
 int error_line[10];
 int error_count = 0;
 int line_count = 1;
+int is_last = 0;
 
 void printerror(char** argv) {
   int i = 0;
   while (error_line[i] != 0) {
     fprintf(stderr, "center: %s: line %d: line too long.\n", argv[1], error_line[i++]);
   }
+}
+
+int allBlank(char *current_line, int count) { 
+  char a = (unsigned char) *current_line;
+  while(1) {
+    a = (unsigned char) *current_line;
+    if (a == '\n' || a == EOF || *current_line == 0) break;
+    if (!isspace(a)) return 0;
+    current_line++;
+  }
+
+  return 1;
 }
 
 char *trimwhitespace(char *str) {
@@ -44,6 +57,7 @@ int main (int argc, char** argv) {
   FILE* input_text = fopen(argv[1], "r");
   FILE* output_file = fopen(argv[2], "w");
   int col_limit = atoi(argv[3]);
+  int past_white = 0;
 
   while(!is_over) {
     chars_in_line =  read_line(input_text, b);
@@ -59,11 +73,20 @@ int main (int argc, char** argv) {
     int greaterHalf;
     if (col_limit % 2 == 1) greaterHalf = 1+col_limit/2;
     else greaterHalf = col_limit/2;
-    fprintf (output_file,"%*s\n", greaterHalf + line_length / 2, current_line);
+
+    int curr_white = allBlank(current_line, line_count);
+    if (curr_white == 0 || past_white == 0) {
+      if (curr_white == 1 && is_over) break;
+      if (!is_last) fprintf (output_file,"%*s\n", greaterHalf + line_length / 2, current_line);
+      else fprintf (output_file,"%*s", greaterHalf + line_length / 2, current_line);
+    }
+
     if (line_length > col_limit) {
       error_line[error_count] = line_count;
       error_count++;
     }
+
+    past_white = curr_white;
     line_count++;
   }
 
