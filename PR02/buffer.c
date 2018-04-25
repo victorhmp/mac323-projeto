a1d57edcd0be1d2f5dec14055e001ea94eadd41a
Ricardo Kojo <ricardokojo7@gmail.com>
2018-04-24 23:11:41 -0300

    Adding comments
    buffer.c: Fixing new size
    center.c: Simplifying printerror(), deleting unused variables, changing variable names

--- PR02/buffer.c
+++ PR02/buffer.c
@@ -4,6 +4,7 @@
 
 #define BUFFER_MIN_SIZE 256
 
+/* variables used at read_line() */
 char in;
 char *insert_pos;
 
@@ -11,7 +12,7 @@ Buffer *buffer_create(size_t member_size) {
   Buffer *buffer = (Buffer*)malloc(sizeof(Buffer));
   if (!buffer) fprintf(stderr, "Memória insuficiente\n");
   
-  buffer->buffer_size = BUFFER_MIN_SIZE;
+  buffer->buffer_size = BUFFER_MIN_SIZE; /* default size of 256 */
   buffer->member_size = member_size;
   buffer->data = malloc(member_size * BUFFER_MIN_SIZE);
   buffer->p = 0;
@@ -21,7 +22,7 @@ Buffer *buffer_create(size_t member_size) {
 
 void buffer_reset(Buffer *B) {
   free(B);
-  B = buffer_create(B->member_size);
+  B = buffer_create(B->member_size); /* maintains member_size */
   B->data = malloc(B->member_size * BUFFER_MIN_SIZE);
   B->p = 0;
 }
@@ -31,27 +32,34 @@ void buffer_destroy(Buffer *B) {
 }
 
 void *buffer_push_back(Buffer *B) {
+  /* increases the buffer_size by 256 if necessary */
   if (B->p == B->buffer_size) {
-    size_t newSize = B->member_size * BUFFER_MIN_SIZE + BUFFER_MIN_SIZE;
+    size_t newSize = B->member_size * (B->buffer_size + BUFFER_MIN_SIZE);
     B->data = realloc(B->data, newSize);
   }
   
+  /* pointer arithmetic to return next free position at buffer */
   return ((char*) B->data + B->p++ * B->member_size); /* casts void* to char* to avoid warning */
 }
 
 int read_line (FILE *input, Buffer *B) {
-  int read_count = 0;
-  int end_line = 0;
-  buffer_reset(B);
+  int read_count = 0; /* number of chars read */
+  int end_line = 0; /* boolean to check if reached end line */
+  buffer_reset(B); /* calls buffer_reset at every read_line call */
+
   while(!end_line) {
+    /* reads next char of input and verifies if it's \n or EOF */
     in = (char)fgetc(input);
     if (in == '\n') {
       end_line = 1;
     }
     if (in == EOF) break;
+
+    /* calls buffer_push_back and inserts char read */
     insert_pos = buffer_push_back(B);
     *insert_pos = in;
     read_count++;
   }
+
   return read_count;
 }
--- PR02/center.c
+++ PR02/center.c
@@ -3,25 +3,19 @@
 #include <ctype.h>
 #include <string.h>
 
-#define MAXTEXTSIZE 1000
-
+/* variables used at main() */
 char *current_line;
-int line_counter = 0;
 int is_over = 0;
 int chars_in_line = 0;
-int error_line[10];
-int error_count = 0;
 int line_count = 1;
-int is_last = 0;
 
-void printerror(char** argv) {
-  int i = 0;
-  while (error_line[i] != 0) {
-    fprintf(stderr, "center: %s: line %d: line too long.\n", argv[1], error_line[i++]);
-  }
+/* prints error at stderr when line_length is bigger than col_limit */
+void printerror(char* file_name, int error_line) {
+  fprintf(stderr, "center: %s: line %d: line too long.\n", file_name, error_line);
 }
 
-int allBlank(char *current_line) { 
+/* verifies if current_line is completely blank */
+int allblank(char *current_line) { 
   char a = (unsigned char) *current_line;
   while(1) {
     a = (unsigned char) *current_line;
@@ -36,13 +30,13 @@ int allBlank(char *current_line) {
 char *trimwhitespace(char *str) {
   char *end;
 
-  /* Trim leading spaces */
+  /* trim leading spaces */
   while(isspace((unsigned char)*str)) str++;
 
   if (*str == 0)
     return str;
 
-  /* Trim trailing spaces */
+  /* trim trailing spaces */
   end = str + strlen(str) - 1;
   while(end > str && isspace((unsigned char)*end)) end--;
 
@@ -57,45 +51,44 @@ int main (int argc, char** argv) {
   FILE* input_text = fopen(argv[1], "r");
   FILE* output_file = fopen(argv[2], "w");
   int col_limit = atoi(argv[3]);
-  int past_white = 0;
 
   int line_length = 0;
-  int greaterHalf = 0;
-  int curr_white = 0;
+  int greater_half = 0;
+
+  /* boolean that check if current and last lines are blank */
+  int curr_blank = 0;
+  int last_blank = 0;
 
   while(!is_over) {
-    chars_in_line =  read_line(input_text, b);
+    chars_in_line = read_line(input_text, b);
     if (chars_in_line == 0) is_over = 1;
-    /* printf("%d\n", chars_in_line); */
 
     current_line = b->data;
-    current_line = trimwhitespace(current_line);
+    current_line = trimwhitespace(current_line); /* trims whitespaces at beggining and end of the line */
 
     line_length = strlen(current_line);
 
-    /* printf("%d\n", line_length); */
+    /* print error when line_length > col_limit */
+    if (line_length > col_limit) printerror(argv[1], line_count);
     
-    if (col_limit % 2 == 1) greaterHalf = 1+col_limit/2;
-    else greaterHalf = col_limit/2;
+    /* calculates the longer half so it centers the line with one extra space to the left if col_limit is odd */
+    if (col_limit % 2 == 1) greater_half = 1 + (col_limit/2);
+    else greater_half = col_limit/2;
 
-    curr_white = allBlank(current_line);
-    if (curr_white == 0 || past_white == 0) {
-      if (curr_white == 1 && is_over) break;
-      if (!is_last) fprintf (output_file,"%*s\n", greaterHalf + line_length / 2, current_line);
-      else fprintf (output_file,"%*s", greaterHalf + line_length / 2, current_line);
+    /* 
+      checks for blank lines
+      if two lines following lines are blank, does not print a second blank line
+     */
+    curr_blank = allblank(current_line);
+    if (curr_blank == 0 || last_blank == 0) {
+      if (curr_blank == 1 && is_over) break;
+      fprintf (output_file,"%*s\n", greater_half + line_length / 2, current_line);
     }
 
-    if (line_length > col_limit) {
-      error_line[error_count] = line_count;
-      error_count++;
-    }
-
-    past_white = curr_white;
+    last_blank = curr_blank;
     line_count++;
   }
 
-  (void)argc; /* unused argument */ 
-
-  printerror(argv);
+  (void)argc; /* to avoid warning because of unused argument */
   return 0;
 }
