58bbed35b3ab83998691a72a08881695b9df60a3
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-05-29 15:25:51 -0300

    Add files from past phases into PR04 folder
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

---
+++ PR04/buffer.c
@@ -0,0 +1,65 @@
+#include "buffer.h"
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_MIN_SIZE 256
+
+/* variables used at read_line() */
+char in;
+char *insert_pos;
+
+Buffer *buffer_create(size_t member_size) {
+  Buffer *buffer = (Buffer*)malloc(sizeof(Buffer));
+  if (!buffer) fprintf(stderr, "Memória insuficiente\n");
+  
+  buffer->buffer_size = BUFFER_MIN_SIZE; /* default size of 256 */
+  buffer->member_size = member_size;
+  buffer->data = malloc(member_size * BUFFER_MIN_SIZE);
+  buffer->p = 0;
+
+  return buffer;
+}
+
+void buffer_reset(Buffer *B) {
+  free(B);
+  B = buffer_create(B->member_size); /* maintains member_size */
+  B->data = malloc(B->member_size * BUFFER_MIN_SIZE);
+  B->p = 0;
+}
+
+void buffer_destroy(Buffer *B) {
+  free(B);
+}
+
+void *buffer_push_back(Buffer *B) {
+  /* increases the buffer_size by 256 if necessary */
+  if (B->p == B->buffer_size) {
+    size_t newSize = B->member_size * (B->buffer_size + BUFFER_MIN_SIZE);
+    B->data = realloc(B->data, newSize);
+  }
+  
+  /* pointer arithmetic to return next free position at buffer */
+  return ((char*) B->data + B->p++ * B->member_size); /* casts void* to char* to avoid warning */
+}
+
+int read_line (FILE *input, Buffer *B) {
+  int read_count = 0; /* number of chars read */
+  int end_line = 0; /* boolean to check if reached end line */
+  buffer_reset(B); /* calls buffer_reset at every read_line call */
+
+  while(!end_line) {
+    /* reads next char of input and verifies if it's \n or EOF */
+    in = (char)fgetc(input);
+    if (in == '\n') {
+      end_line = 1;
+    }
+    if (in == EOF) break;
+
+    /* calls buffer_push_back and inserts char read */
+    insert_pos = buffer_push_back(B);
+    *insert_pos = in;
+    read_count++;
+  }
+
+  return read_count;
+}
---
+++ PR04/stable.c
@@ -0,0 +1,119 @@
+#include "stable.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define  M 7919
+
+// hash function
+int hash(const char *key) {
+    int h = 0;
+    for (unsigned int i = 0; i < strlen(key); i++)
+      h = (31 * h + (int)key[i]) % M;
+    return h;
+}
+
+// linked list node: string (key), EntryData(val) and prev node pointer
+typedef struct Node {
+    const char *key;
+    EntryData *val;
+    struct Node *prev;
+} Node;
+
+/* Symbol table comprises a single array
+   array elements are pointers to the last nodes of each linked list */
+struct stable_s {
+    Node *st[M];  
+};
+
+SymbolTable stable_create() {
+    SymbolTable a = malloc(sizeof(struct stable_s));
+    *(a->st) = malloc(sizeof(M * sizeof(Node)));
+    for (int i = 0 ; i < M; i++) {
+        Node *insert = NULL;
+        a->st[i] = insert;
+    }
+
+    return a;
+}
+
+void stable_destroy(SymbolTable table) {
+    for(int i = 0; i < M; i++) {
+        free(table->st[i]);
+    }
+    free(table);
+}
+
+InsertionResult stable_insert(SymbolTable table, const char *key) {
+    EntryData *find_result = stable_find(table, key);
+    InsertionResult insert_result;
+
+    if (find_result != NULL) {
+        insert_result.new = 0;
+        insert_result.data = find_result;
+    } 
+    else {
+        insert_result.new = 1;
+        
+        int h = hash(key);
+        Node *new_node = table->st[h];
+        new_node = malloc(sizeof(Node));
+
+        char *store = malloc((strlen(key)+1)*sizeof(char));
+        int i = 0;
+        while(*key != '\0') {
+            *store = *key;
+            key++;
+            store++;
+            i++;
+        }
+        store -= i;
+        key -= i;
+        new_node->key = store;
+
+        new_node->prev = table->st[h];
+        new_node->val = malloc(sizeof(EntryData));
+        table->st[h] = new_node;
+
+        insert_result.data = new_node->val;
+    }
+
+    return insert_result;
+}
+
+EntryData *stable_find(SymbolTable table, const char *key){
+    if (table->st[hash(key)] == NULL){
+        return NULL;
+    }
+
+    Node *nd = table->st[hash(key)];
+    while (nd) {
+        if (strcmp(nd->key, key) == 0) {
+            EntryData *return_pointer = nd->val;
+            return return_pointer;
+        }
+        nd = nd->prev;
+    }
+    return NULL;
+}
+
+int recurs_visit(Node *node, int (*visit)(const char *key, EntryData *data)) {
+    int visit_res = visit(node->key, node->val);
+    if (visit_res == 0) return 0;
+    if (node->prev) {
+        recurs_visit(node->prev, visit);
+    }
+    return 1;
+}
+
+int stable_visit(SymbolTable table, 
+                    int (*visit)(const char *key, EntryData *data)) {
+    for (unsigned int i = 0; i < M; i++) {
+        if (table->st[i] != NULL){ 
+            int visit_res = recurs_visit(table->st[i], visit);
+            if(visit_res == 0) return 0;
+        }
+    }
+
+    return 1;
+}
