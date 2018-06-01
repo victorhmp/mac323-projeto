c40209ea5677fa67a4c196e2fdecb66a1673f3a6
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-06-01 15:24:52 -0300

    Make `parse_test` print operand types
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- PR04/parse_test.c
+++ PR04/parse_test.c
@@ -13,7 +13,7 @@ char *current_line = 0;
 
 /* Variables for parser return and alias_table */
 int parse_result;
-char  *errptr = 0;
+char *errptr = 0;
 Instruction *instr = 0;
 
 // void create_alias(SymbolTable *st, InsertionResult insert_res) {
@@ -21,77 +21,80 @@ Instruction *instr = 0;
 // }
 
 char *get_type_string(Operand *operand) {
-    if (operand->type == REGISTER) 
-      return "Register";
-    if (operand->type == LABEL) 
-      return "Label";
-    if (operand->type == NUMBER_TYPE) 
-      return "Number";
-    if (operand->type == STRING) 
-      return "String";
+    if (operand->type == REGISTER)
+        return "Register";
+    if (operand->type == LABEL)
+        return "Label";
+    if (operand->type == NUMBER_TYPE)
+        return "Number";
+    if (operand->type == STRING)
+        return "String";
+    else
+        return "None";
 }
 
-int main(int argc, char** argv) {
-  FILE* input_text = fopen(argv[1], "r");
-  Buffer *b = buffer_create(sizeof(char));
-  SymbolTable stable = stable_create();
+int main(int argc, char **argv) {
+    FILE *input_text = fopen(argv[1], "r");
+    Buffer *b = buffer_create(sizeof(char));
+    SymbolTable stable = stable_create();
 
-  while(read_line(input_text, b)) {
-    current_line = b->data;
-    // printf("%s\n", current_line);
+    while (read_line(input_text, b)) {
+        current_line = b->data;
+        // printf("%s\n", current_line);
 
-    const char  **errptr = malloc(sizeof(const char**));
-    Instruction **instr = malloc(sizeof(Instruction**));
-    parse_result = parse(current_line, stable, instr, errptr);
+        const char **errptr = malloc(sizeof(const char **));
+        Instruction **instr = malloc(sizeof(Instruction **));
+        parse_result = parse(current_line, stable, instr, errptr);
 
-    Instruction f = **instr;
-    
-    if (!parse_result) {
-        printf("Error on: %s\n", *errptr);
-        break;
-    }
-    else {
+        Instruction f = **instr;
 
-      if (strcmp(f.op->name, "IS") == 0) {
-        InsertionResult insert;
-        insert.new = -1;
-        insert.data = NULL;
-        
-        insert = stable_insert(stable, f.label);
-        if (insert.new == 0) printf("ERROR: Duplicate symbol\n");
+        if (!parse_result) {
+            printf("Error on: %s\n", *errptr);
+            break;
+        }
         else {
-          EntryData alias;
-          alias.opd = f.opds[0];
-          *(insert.data) = alias;
+
+            if (strcmp(f.op->name, "IS") == 0) {
+                InsertionResult insert;
+                insert.new = -1;
+                insert.data = NULL;
+
+                insert = stable_insert(stable, f.label);
+                if (insert.new == 0)
+                    printf("ERROR: Duplicate symbol\n");
+                else {
+                    EntryData alias;
+                    alias.opd = f.opds[0];
+                    *(insert.data) = alias;
+                }
+            }
+
+            printf("line     = %s", current_line);
+            if (f.label == NULL) {
+                f.label = "n/a";
+                printf("label    = n/a\n");
+            }
+            else {
+                char line_label[strlen(f.label) + 1];
+                strcpy(line_label, f.label);
+                printf("label    = %s\n", line_label);
+            }
+            printf("operator = %s\n", f.op->name);
+            printf("operands = ");
+
+            for (int i = 0; i < 3; i++) {
+                if (f.opds[i]->value.str != NULL) {
+                    printf("%s (%s), ", get_type_string(f.opds[i]), f.opds[i]->value.str);
+                }
+            }
+            printf("\n\n");
+
+            free(errptr);
+            free(instr);
         }
-      }
-
-      printf("line     = %s", current_line);
-      if (f.label == NULL){
-          f.label = "n/a";
-          printf("label    = n/a\n");
-      }
-      else {
-          char line_label[strlen(f.label)+1];
-          strcpy(line_label, f.label);
-          printf("label    = %s\n", line_label);
-      }
-      printf("operator = %s\n", f.op->name);
-      printf("operands = ");
-
-      for (int i=0;i<3;i++) {
-        if (f.opds[i]->value.str != NULL) {
-          printf("%s ,", f.opds[i]->value.str);
-        }
-      }
-      printf("\n\n");
-
-      free(errptr);
-      free(instr);
     }
-  }
 
-  buffer_destroy(b);
+    buffer_destroy(b);
 
-  return 0;
+    return 0;
 }
